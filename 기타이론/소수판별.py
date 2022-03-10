08c679f20d52ac51dc9fd2f91b9b3004bce91a80
room9 <miiny3524@gmail.com>
2022-03-10 13:51:55 +0900

    NDB DP / 최단거리 / 그래프이론 / 소수/ 투포인트 / 순열과조합

---
+++ 기타이론/소수판별.py
@@ -0,0 +1,32 @@
+import math
+
+# 소수 판별 함수
+def is_prime_number(x):
+    # 2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(math.sqrt(x)) + 1):
+        # x가 해당 수로 나누어떨어진다면
+        if x % i == 0:
+            return False # 소수가 아님
+    return True # 소수임
+
+print(is_prime_number(4)) # 4는 소수가 아님
+print(is_prime_number(7)) # 7은 소수임
+
+
+#-----
+# 약수 갯수 구하기 : 루트값의 약수 구하고 2배 , 제곱수인경우 2배 -1
+
+def getMyDivisor(n):
+
+    divisorsList = []
+
+    for i in range(1, int(n**(1/2)) + 1):
+        if (n % i == 0):
+            divisorsList.append(i) 
+            if ( (i**2) != n) : 
+                divisorsList.append(n // i)
+
+    divisorsList.sort()
+    
+    return divisorsList
+    
